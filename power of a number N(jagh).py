2bb4477da677eb1aaef403271b2968472e66f9c3
jaghadish281098 <47181280+jaghadish281098@users.noreply.github.com>
2019-02-11 21:13:09 +0530

    Add files via upload

---
+++ power of a number N(jagh).py
@@ -0,0 +1,1294 @@
+Python 2.7.9 (default, Sep 17 2016, 20:26:04) 
+[GCC 4.9.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+n,k=raw_input().split()
+res=1
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 2, in <module>
+    n,k=raw_input().split()
+ValueError: need more than 1 value to unpack
+>>> ================================ RESTART ================================
+>>> 
+5 2
+25
+>>> ================================ RESTART ================================
+>>> 
+121
+yes
+>>> ================================ RESTART ================================
+>>> 
+254
+no
+>>> ================================ RESTART ================================
+>>> 
+5
+
+Traceback (most recent call last):
+  File "/home/pi/prime or not(jagh).py", line 3, in <module>
+    for i in range(2,n-1):
+TypeError: unsupported operand type(s) for -: 'str' and 'int'
+>>> ================================ RESTART ================================
+>>> 
+5
+prime
+>>> 4
+4
+>>> ================================ RESTART ================================
+>>> 
+4
+not prime
+>>> ================================ RESTART ================================
+>>> 
+5
+
+Traceback (most recent call last):
+  File "/home/pi/prime or not(jagh).py", line 3, in <module>
+    for i in range(2,n-1):
+TypeError: unsupported operand type(s) for -: 'str' and 'int'
+>>> ================================ RESTART ================================
+>>> 
+7
+prime
+>>> ================================ RESTART ================================
+>>> 
+4
+not prime
+>>> ================================ RESTART ================================
+>>> 
+0 1
+0
+>>> ================================ RESTART ================================
+>>> 
+0 0
+0
+>>> ================================ RESTART ================================
+>>> 
+2 5
+2
+4
+8
+16
+32
+>>> ================================ RESTART ================================
+>>> 
+5 1
+>>> ================================ RESTART ================================
+>>> 
+5 10
+6
+7
+8
+9
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+12
+13
+14
+15
+16
+17
+18
+19
+>>> ================================ RESTART ================================
+>>> 
+5 10
+6
+7
+8
+9
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+13
+15
+17
+19
+>>> ================================ RESTART ================================
+>>> 
+20 40
+22
+24
+26
+28
+30
+32
+34
+36
+38
+>>> ================================ RESTART ================================
+>>> 
+20 30
+
+Traceback (most recent call last):
+  File "/home/pi/prime or not bw two intrerval(jagh).py", line 3, in <module>
+    for i in range(n+1,k):
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> ================================ RESTART ================================
+>>> 
+20 30
+>>> ================================ RESTART ================================
+>>> 
+10 20
+>>> ================================ RESTART ================================
+>>> 
+10 20
+2
+5
+2
+5
+2
+5
+2
+5
+2
+5
+2
+5
+2
+5
+2
+5
+2
+5
+>>> ================================ RESTART ================================
+>>> 
+1 20
+>>> ================================ RESTART ================================
+>>> 
+10 20
+>>> ================================ RESTART ================================
+>>> 
+10 20
+2
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+12
+13
+14
+15
+16
+17
+18
+19
+>>> ================================ RESTART ================================
+>>> 
+10 20
+
+Traceback (most recent call last):
+  File "/home/pi/prime or not bw two intrerval(jagh).py", line 1, in <module>
+    n=int(raw_input())
+ValueError: invalid literal for int() with base 10: '10 20'
+>>> ================================ RESTART ================================
+>>> 
+20 30
+2
+>>> ================================ RESTART ================================
+>>> 
+20 30
+29
+>>> ================================ RESTART ================================
+>>> 
+20 30
+29
+>>> ================================ RESTART ================================
+>>> 
+10 20
+19
+>>> ================================ RESTART ================================
+>>> 
+20 30
+21
+22
+23
+24
+25
+26
+27
+28
+29
+>>> ================================ RESTART ================================
+>>> 
+20 30
+21
+22
+23
+24
+25
+26
+27
+28
+29
+>>> ================================ RESTART ================================
+>>> 
+20 30
+21
+22
+23
+24
+25
+26
+27
+28
+29
+>>> ================================ RESTART ================================
+>>> 
+20 30
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+20 30
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+20 30
+28
+28
+28
+28
+28
+28
+28
+28
+28
+>>> ================================ RESTART ================================
+>>> 
+20 30
+29
+>>> ================================ RESTART ================================
+>>> 
+10 20
+19
+>>> ================================ RESTART ================================
+>>> 
+20 30
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+>>> ================================ RESTART ================================
+>>> 
+20 30
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+>>> ================================ RESTART ================================
+>>> 
+10 20
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+no prime no.
+>>> ================================ RESTART ================================
+>>> 
+20 30
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+>>> ================================ RESTART ================================
+>>> 
+20 30
+21
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+22
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+23
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+24
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+25
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+26
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+27
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+28
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+29
+>>> ================================ RESTART ================================
+>>> 
+20 30
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+there is no prime no.
+>>> ================================ RESTART ================================
+>>> 
+10 20
+19
+19
+19
+19
+19
+19
+19
+19
+19
+>>> ================================ RESTART ================================
+>>> 
+20 30
+20
+20
+20
+20
+20
+20
+20
+20
+20
+>>> ================================ RESTART ================================
+>>> 
+10 20
+>>> ================================ RESTART ================================
+>>> 
+20 30
+>>> 
+>>> 
+>>> 20 60
+SyntaxError: invalid syntax
+>>> ================================ RESTART ================================
+>>> 
+10 20
+>>> ================================ RESTART ================================
+>>> 
+20 30
+>>> ================================ RESTART ================================
+>>> 
+20 30
+>>> ================================ RESTART ================================
+>>> 
+20 30
+>>> ================================ RESTART ================================
+>>> 
+1 10
+3
+4
+4
+5
+5
+5
+6
+6
+6
+6
+7
+7
+7
+7
+7
+8
+8
+8
+8
+8
+8
+9
+9
+9
+9
+9
+9
+9
+10
+10
+10
+10
+10
+10
+10
+10
+>>> ================================ RESTART ================================
+>>> 
+20 30
+23
+29
+>>> ================================ RESTART ================================
+>>> 
+20 30
+21
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+25
+25
+25
+27
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+11
+11
+11
+11
+11
+11
+11
+11
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+15
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+>>> ================================ RESTART ================================
+>>> 
+20 30
+
+Traceback (most recent call last):
+  File "/home/pi/jagh.py", line 3, in <module>
+    for i in range(2,j):
+NameError: name 'j' is not defined
+>>> ================================ RESTART ================================
+>>> 
+20 30
+21
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+25
+25
+25
+27
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+>>> ================================ RESTART ================================
+>>> 
+20 30
+21
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+23
+25
+25
+25
+27
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+29
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+11
+11
+11
+11
+11
+11
+11
+11
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+15
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+13
+17
+19
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+11
+11
+11
+11
+11
+11
+11
+11
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+15
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+11
+11
+11
+11
+11
+11
+11
+11
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+15
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+13
+17
+19
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+13
+17
+19
+>>> ================================ RESTART ================================
+>>> 
+10 20
+11
+11
+11
+11
+11
+11
+11
+11
+11
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+13
+15
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+17
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+19
+>>> ================================ RESTART ================================
+>>> 
+20 30
+23
+29
+>>> ================================ RESTART ================================
+>>> 
+20 30
+23
+29
+>>> ================================ RESTART ================================
+>>> 
+11 20
+13
+17
+19
+>>> ================================ RESTART ================================
+>>> 
+20 30
+23
+29
+>>> ================================ RESTART ================================
+>>> 
+11 20
+13
+17
+19
+>>> ================================ RESTART ================================
+>>> 
+
+Traceback (most recent call last):
+  File "/home/pi/armstrong or not(jagh).py", line 1, in <module>
+    n=int(raw_input)
+TypeError: int() argument must be a string or a number, not 'builtin_function_or_method'
+>>> ================================ RESTART ================================
+>>> 
+2
+8
+>>> ================================ RESTART ================================
+>>> 
+271
+not an armstrong no.
+>>> ================================ RESTART ================================
+>>> 
+371
+not an armstrong no.
+>>> ================================ RESTART ================================
+>>> 
+371
+not an armstrong no.
+>>> ================================ RESTART ================================
+>>> 
+121
+10
+>>> ================================ RESTART ================================
+>>> 
+121
+10
+not an armstrong number
+>>> ================================ RESTART ================================
+>>> 
+371
+371
+not an armstrong number
+>>> ================================ RESTART ================================
+>>> 
+121
+10
+>>> ================================ RESTART ================================
+>>> 
+371
+371
+>>> 111
+111
+>>> ================================ RESTART ================================
+>>> 
+2 7 1
+
+Traceback (most recent call last):
+  File "/home/pi/armstrong or not(jagh).py", line 1, in <module>
+    n=int(raw_input())
+ValueError: invalid literal for int() with base 10: '2 7 1'
+>>> ================================ RESTART ================================
+>>> 
+134
+92
+>>> ================================ RESTART ================================
+>>> 
+153
+153
+>>> ================================ RESTART ================================
+>>> 
+753
+495
+>>> ================================ RESTART ================================
+>>> 
+121
+10
+not armstrong
+>>> 
